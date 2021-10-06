b438da60d6744ac186b37dc727ba3ddf3c9e6b87
Ryan Brown <ryan.a.brown191@gmail.com>
2021-10-05 21:36:58 -0400

    Add functions for random choice of restaurant, transportation and entertainment

--- DTG.js
+++ DTG.js
@@ -1,7 +1,23 @@
-// Destination selected randomly
 function randomDest(dest){
     return dest[Math.floor(Math.random()*dest.length)];
 }
-
 let dest = ["Charlotte", "Virginia Beach", "Washington D.C."];
 console.log(randomDest(dest));
+// Destination selected randomly
+function randomTrans(trans){
+    return trans[Math.floor(Math.random()*trans.length)];
+}
+let trans = ["Bus", "Train", "Car"];
+console.log(randomTrans(trans));
+// Transportation selected randomly
+function randomEnter(enter){
+    return enter[Math.floor(Math.random()*enter.length)];
+}
+let enter = ["Movies", "Park", "Museum"];
+console.log(randomEnter(enter));
+// Entertainment selected randomly
+function randomRest(rest){
+    return rest[Math.floor(Math.random()*rest.length)];
+}
+let rest = ["Olive Garden", "Red Lobster", "Outback"];
+console.log(randomRest(rest));
